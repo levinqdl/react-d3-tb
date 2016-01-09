841d00d6160512ead24c33e9cf30d1c5c57720e0
levinqdl <levinqdl@gmail.com>
2016-01-09 20:56:03 +0800

    init

---
+++ mixins/TooltipMixin.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var React = require('react');
+
+module.exports =  {
+
+  propTypes: {
+    showTooltip:    React.PropTypes.bool,
+    tooltipFormat:  React.PropTypes.func
+  },
+
+  getDefaultProps:function() {
+    return {
+      showTooltip:   true,
+      tooltipFormat: function(d)  {return String(d.yValue);}
+    };
+  },
+
+  getInitialState:function() {
+    return {
+      tooltip: {
+        x: 0,
+        y: 0,
+        child: '',
+        show: false
+      }
+    };
+  },
+
+  onMouseOver:function(x, y, dataPoint) {
+    if(!this.props.showTooltip)
+      return;
+    this.setState({
+      tooltip: {
+        x: x,
+        y: y,
+        child: this.props.tooltipFormat.call(this, dataPoint),
+        show: true 
+      } 
+    });
+  },
+
+  onMouseLeave:function() {
+    if(!this.props.showTooltip)
+      return;
+    this.setState({
+      tooltip: {
+        x: 0,
+        y: 0,
+        child: '',
+        show: false
+      } 
+    });
+  }
+}
